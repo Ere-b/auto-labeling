8305505ba08df77d583e2876a8984d5a252dbf29
Hanul <110656247+hanul-dev@users.noreply.github.com>
2023-08-04 13:34:46 +0900

    Add weapon class

---
+++ weapons.py
@@ -0,0 +1,11 @@
+from enum import Enum
+# 1. c_knife 2. knife 3. pistol 4. gun 5. unknown 6 uzi 7. empty
+class Weapon(Enum):
+    c_knife = 0
+    knife = 1
+    pistol = 2
+    gun = 3
+    unknown = 4
+    uzi = 5
+    empty = 6
+    
