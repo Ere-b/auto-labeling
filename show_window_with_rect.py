631d9b967815cc735ba59ecb6fd98725731b55c4
Hanul <110656247+hanul-dev@users.noreply.github.com>
2023-08-07 18:20:14 +0900

    Feat 이미지 윈도우 위치 조정

--- show_window_with_rect.py
+++ show_window_with_rect.py
@@ -5,15 +5,17 @@ def show_window_with_rect(image_with_rect, image_path, x, y, w, h):
   객체가 감지된 이미지를 보여주는 Function
 
   Args:
-      image_path (str): _description_
-      x (int): _description_
-      y (int): _description_
-      w (int): _description_
-      h (int): _description_
+      image_with_rect (str): 객체 검출 결과물
+      image_path (str): 이미지 경로
+      x (int): value of x
+      y (int): value of y
+      w (int): width of rectangle
+      h (int): height of rectangle
   """
   cv2.rectangle(image_with_rect, (x, y), (x + w, y + h), (0, 0, 255), 1)
   cv2.imshow(image_path, image_with_rect)
   cv2.namedWindow(image_path, cv2.WINDOW_NORMAL)
   cv2.resizeWindow(image_path, 640, 480)
+  cv2.moveWindow(image_path, 500,0)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
